8c8134e6652eca9e7a20b68cdcd3567a832a7cac
laxatzi <laxatzi@yahoo.com>
2019-04-09 16:49:28 +0300

    add close and open functions to js file

--- cssgrid/JavaScript/gallery3.js
+++ cssgrid/JavaScript/gallery3.js
@@ -2,3 +2,13 @@ const gallery = document.querySelector('.gallery');
 const overlay = document.querySelector('.overlay');
 const overlayImage = overlay.querySelector('img');
 const overlayClose = overlay.querySelector('.close');
+
+function handleClick(e) {
+  const src = e.currentTarget.querySelector('img').src;
+  overlayImage.src = src;
+  overlay.classList.add('open');
+}
+
+function close() {
+  overlay.classList.remove('open');
+}
