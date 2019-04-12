a25aaa5dcb30a5f31e2c2d86d2d2414f7f09ab69
laxatzi <laxatzi@yahoo.com>
2019-04-12 13:31:15 +0300

    make js function work

--- cssgrid/JavaScript/gallery4.js
+++ cssgrid/JavaScript/gallery4.js
@@ -3,4 +3,19 @@
 // fds adds 'open' class to overlay div so to make apear
 //when i click on .close a function close the overlay div
 
+const galleryItem = document.querySelector('.item');
+const overlay = document.querySelector('.overlay')
+const overlayClose = overlay.querySelector('.close');
 
+
+function open() {
+   overlay.classList.add('open');
+ 
+}
+function close() {
+   overlay.classList.remove('open');
+
+}
+
+galleryItem.addEventListener('click', open);
+overlayClose.addEventListener('click', close);
