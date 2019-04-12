c66de01addda5ef2396f99f320ac838e910cac0d
laxatzi <laxatzi@yahoo.com>
2019-04-12 13:12:44 +0300

    pseudocode on js file

--- cssgrid/JavaScript/gallery4.js
+++ cssgrid/JavaScript/gallery4.js
@@ -1,1 +1,6 @@
+// when i click on .item a function does something 
+// fds is function action
+// fds adds 'open' class to overlay div so to make apear
+//when i click on .close a function close the overlay div
+
 
